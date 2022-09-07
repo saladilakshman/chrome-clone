3ce895f708465b85cd2b8d6c8a9df33cb39bc401
saladilakshman <103055304+saladilakshman@users.noreply.github.com>
2022-09-07 13:56:11 +0530

    Updates

---
+++ precache-manifest.6eab680ab18adf8d9b96a58f2f86a25a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a4f07c4560244e9471e",
+    "url": "/chrome-clone/static/css/main.9b205b97.chunk.css"
+  },
+  {
+    "revision": "9a4f07c4560244e9471e",
+    "url": "/chrome-clone/static/js/main.d25685a7.chunk.js"
+  },
+  {
+    "revision": "96056992ded17166ee9e",
+    "url": "/chrome-clone/static/js/runtime~main.a6a2cd83.js"
+  },
+  {
+    "revision": "3d839661791ec3908a14",
+    "url": "/chrome-clone/static/css/2.b7aad512.chunk.css"
+  },
+  {
+    "revision": "3d839661791ec3908a14",
+    "url": "/chrome-clone/static/js/2.0059b713.chunk.js"
+  },
+  {
+    "revision": "7b4b590765ba9cc9d7de",
+    "url": "/chrome-clone/static/js/3.f1bbbdc7.chunk.js"
+  },
+  {
+    "revision": "b344b0d40a3beff31647f576fef5ab7c",
+    "url": "/chrome-clone/index.html"
+  }
+];
