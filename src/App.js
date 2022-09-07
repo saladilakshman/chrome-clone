aad2fa464693cd388614013f07da086eda1b3e25
saladilakshman <103055304+saladilakshman@users.noreply.github.com>
2022-09-07 13:35:45 +0530

    added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React,{useState,useEffect} from "react";
 import moment from "moment";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {FaHome,FaEllipsisV,FaSearch,FaShare} from "react-icons/fa";
+import {FaHome,FaEllipsisV,FaSearch,FaShare,FaMicrophone} from "react-icons/fa";
 import {GoGear} from "react-icons/go";
 function App() {
   const[searchValue,setSearchValue]=useState('');
+  const[close,setClose]=useState();
   const[news,setNews]=useState([]);
   const getNewsData=async()=>{
 const response=await fetch(' https://gnews.io/api/v4/top-headlines?country=ind&lang=en&token=4a90d41250afd1a3634aedf39dc2b621');
@@ -16,6 +17,29 @@ setNews(data.articles);
 
   }
   useEffect(()=>{getNewsData();},[]);
+  const voiceFunctioning=()=>{
+    if(!close){
+      setClose(preState=>!preState);
+     startRecogs();
+    }
+    else{
+      console.log('failed to launch voice recognition')
+    }
+  }
+  const startRecogs=()=>{
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const recog = new SpeechRecognition();
+    recog.continuous=false;
+    recog.interimresults=false;
+    recog.onstart=()=>{
+      console.log("listening...");
+    }
+    recog.onresult=(event)=>{
+      const result=event.results[0][0].transcript;
+      window.location.href=`https://www.${result}com`;
+    }
+    recog.start();
+  }
   return (
     <div className="phone">
    <div className="navbar pt-3">
@@ -26,13 +50,31 @@ setNews(data.articles);
    </div>
    </div>
    <img className="w-50 h-auto "id="logo"src="https://www.localplaces.de/wp-content/uploads/2017/09/google-tools-1.png"alt=""/>
-    <div>
-      <div className="input-group d-flex justify-content-center align-items-center mx-auto">
-        <span  className="input-group-text"><FaSearch className=""id="search"onClick={()=>window.location.href=`https://www.${searchValue}.com`}/></span>
-      <input type="text"value={searchValue} onChange={(e)=>setSearchValue(e.target.value)}/>
-      </div>
-    </div>
    
+        <div className="input-group d-flex justify-content-center align-items-center mx-auto"id="field">
+          <span className="input-group-text">
+            <FaSearch
+              className=""
+              id="search"
+              onClick={() =>
+                (window.location.href = `https://www.${searchValue}.com`)
+              }
+            />
+          </span>
+          <input
+            type="text"
+            value={searchValue}
+            placeholder="search here"
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+          <span className="input-group-text"onClick={voiceFunctioning}><FaMicrophone/></span>
+        </div>
+     { close &&<div className=" card w-75 ms-5 pb-3"id="card">
+        <div className="d-flex justify-content-center align-items-center">
+        <img  className="w-50"src="https://tekiota.com/wp-content/uploads/2015/10/Google-Microphone-for-Speech-Recognition1.png"alt=""/>
+      </div>
+      <p className="text-center">Listening...</p>
+      </div>}
     <section className="mt-3">
 <div className="images">
 <img src="https://th.bing.com/th/id/R.3d88a927f8529dcba03364b09d98adbe?rik=JYmQaMVSULpYQg&riu=http%3a%2f%2fwww.thewowstyle.com%2fwp-content%2fuploads%2f2015%2f01%2fnature-images.jpg&ehk=KAqZpO4yRo6V5YA75CVrNPO1UNoFiHpznXqWnLl2ex4%3d&risl=&pid=ImgRaw&r=0"alt=""className="w-50"id="im"/> 
@@ -48,8 +90,8 @@ setNews(data.articles);
     </div>
     {news.map((items)=>{
       const{id,image,publishedAt,source,url,title}=items;
-      const myDate=moment(publishedAt).fromNow();
-      const newDate=myDate.slice(0,3);
+      const myDate=moment(publishedAt).startOf('day').fromNow();
+      const newDate=myDate.slice(0,4);
       const name=source.name;
       return(
         <div className="d-flex justify-content-center align-items-center">
@@ -78,6 +120,6 @@ setNews(data.articles);
     })}
     </div>
   );
-}
+  }
 
 export default App;
